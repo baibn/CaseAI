d3d92ecbe97a13ffc5985232ac32c0df4ab42169
Baiboning <2523581400@qq.com>
2025-09-03 14:21:27 +0800

    将 client 转为普通目录

---
+++ client/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import HomeNew from '../views/HomeNew.vue'
+import ProjectDetail  from "../views/ProjectDetail.vue";
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/homenew',
+    component: HomeNew,
+  },
+  {
+    path: '/detail/',
+    component: ProjectDetail,
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
